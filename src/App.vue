145846df9f8e6dc7cccbe08ea7c87f3c22c44cd6
Mo Jiwa <mohammed.jiwa@gmail.com>
2022-09-13 21:47:52 +0100

    Complete simple landing page

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import Landing from './components/Landing.vue';
 </script>
 
 <template>
-  <div class="bg-stone-800 h-screen text-white font-sans text-2xl flex justify-center text-center p-4">
-    <Landing msg="Coming soon..." />
+  <div>
+    <Landing />
   </div>
 </template>
 
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,16 +1,93 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-defineProps<{ msg: string }>()
-
-</script>
-
 <template>
-  <div class="w-1/5">
-    <img class="w-1/2 mx-auto" src="/robot.svg" alt="MoBot logo"/>
-    <h1>{{ msg }}</h1>
+ <div class="relative xl:container xl:mx-auto overflow-hidden bg-white rounded-t-lg">
+    <div class="mx-auto max-w-full">
+      <div class="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+        <svg class="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+          <polygon points="50,0 100,0 50,100 0,100" />
+        </svg>
+
+        <Popover>
+          <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
+            <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+              <div class="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
+                <div class="flex w-full items-center justify-between md:w-auto">
+                  <a href="#">
+                    <span class="sr-only">MoBot</span>
+                    <img alt="MoBot Logo" class="h-8 w-auto sm:h-10" src="/robot.svg" />
+                  </a>
+                  <div class="-mr-2 flex items-center md:hidden">
+                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span class="sr-only">Open main menu</span>
+                      <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+                    </PopoverButton>
+                  </div>
+                </div>
+              </div>
+              <div class="hidden md:ml-10 md:block md:space-x-8 md:pr-4">
+                <a v-for="item in navigation" :key="item.name" :href="item.href" class="font-medium text-gray-500 hover:text-gray-900">{{ item.name }}</a>
+                <a href="#" target="_blank" class="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
+              </div>
+            </nav>
+          </div>
+
+          <transition enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
+            <PopoverPanel focus class="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden">
+              <div class="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+                <div class="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img class="h-8 w-auto" src="/robot.svg" alt="" />
+                  </div>
+                  <div class="-mr-2">
+                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span class="sr-only">Close main menu</span>
+                      <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                    </PopoverButton>
+                  </div>
+                </div>
+                <div class="space-y-1 px-2 pt-2 pb-3">
+                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">{{ item.name }}</a>
+                </div>
+                <a href="#" target="_blank" class="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100">Log in</a>
+              </div>
+            </PopoverPanel>
+          </transition>
+        </Popover>
+
+        <main class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <div class="sm:text-center lg:text-left">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+              <span class="block xl:inline">The only Discord bot</span>
+              {{ ' ' }}
+              <span class="block text-indigo-600 xl:inline">you need</span>
+            </h1>
+            <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Imagine if you could have one Discord bot to manage role reactions, pronoun picking, custom commands, streaming alerts, and more! All at a fraction of the cost compared to the competition.</p>
+            <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">MoBot is coming soon...</p>
+            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div class="rounded-md shadow">
+                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Add Bot</a>
+              </div>
+              <div class="mt-3 sm:mt-0 sm:ml-3">
+                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg">Learn More</a>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full" src="https://safety.discord.com/hc/article_attachments/360059655071/platforms.png" alt="Bot example image" />
+    </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+  import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+  import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+  
+  const navigation = [
+    { name: 'Features', href: '#' },
+    { name: 'Pricing', href: '#' },
+    { name: 'Documentation', href: '#' },
+    { name: 'Support', href: '#' },
+  ]
+  </script>
