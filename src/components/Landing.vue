1e1d697ede51108eeeb669a540b31ddf469e7955
Mo Jiwa <mohammed.jiwa@gmail.com>
2022-09-20 21:44:37 +0100

    set colours

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,22 +1,18 @@
 <template>
- <div class="relative xl:container xl:mx-auto overflow-hidden bg-white rounded-t-lg">
+ <div class="relative container mx-auto overflow-hidden bg-white rounded-t-lg">
     <div class="mx-auto max-w-full">
-      <div class="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+      <div class="relative z-10 bg-white w-full pb-32">
         <svg class="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
           <polygon points="50,0 100,0 50,100 0,100" />
         </svg>
 
         <Popover>
-          <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
-            <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-              <div class="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
-                <div class="flex w-full items-center justify-between md:w-auto">
-                  <!-- <a href="#">
-                    <span class="sr-only">MoBot</span>
-                    <img alt="MoBot Logo" class="h-24 w-auto sm:h-24 p-2" src="/robot.svg" />
-                  </a> -->
+          <div class="relative pt-6 px-8">
+            <nav class="relative flex items-center h-10 justify-start" aria-label="Global">
+              <div class="flex flex-shrink-0 items-center flex-grow-0">
+                <div class="flex w-full items-center justify-between">
                   <div class="-mr-2 flex items-center md:hidden">
-                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-mobot-gray-200 hover:bg-mobot-gray-100 hover:text-mobot-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-mobot-purple-400">
                       <span class="sr-only">Open main menu</span>
                       <Bars3Icon class="h-6 w-6" aria-hidden="true" />
                     </PopoverButton>
@@ -24,8 +20,8 @@
                 </div>
               </div>
               <div class="hidden md:ml-10 md:block md:space-x-8 md:pr-4">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" class="font-medium text-gray-500 hover:text-gray-900">{{ item.name }}</a>
-                <a href="#" target="_blank" class="font-medium text-indigo-600 hover:text-indigo-500">Log in</a>
+                <a v-for="item in navigation" :key="item.name" :href="item.href" class="font-medium text-mobot-dark-gray-200 hover:text-mobot-dark-gray-300">{{ item.name }}</a>
+                <a href="#" target="_blank" class="font-medium text-mobot-purple-400 hover:text-mobot-purple-300">Log in</a>
               </div>
             </nav>
           </div>
@@ -38,47 +34,45 @@
                     <img class="h-8 w-auto" src="/robot-face.svg" alt="" />
                   </div>
                   <div class="-mr-2">
-                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-mobot-gray-200 hover:bg-mobot-gray-100 hover:text-mobot-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-mobot-purple-400">
                       <span class="sr-only">Close main menu</span>
                       <XMarkIcon class="h-6 w-6" aria-hidden="true" />
                     </PopoverButton>
                   </div>
                 </div>
                 <div class="space-y-1 px-2 pt-2 pb-3">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900">{{ item.name }}</a>
+                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-mobot-dark-gray-200 hover:bg-mobot-gray-100 hover:text-mobot-dark-gray-300">{{ item.name }}</a>
                 </div>
-                <a href="#" target="_blank" class="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100">Log in</a>
+                <a href="#" target="_blank" class="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-mobot-purple-400 hover:bg-mobot-gray-100">Log in</a>
               </div>
             </PopoverPanel>
           </transition>
         </Popover>
 
-        <main class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-          <div class="sm:text-center lg:text-left">
+        <main class="mx-auto max-w-7xl px-8 mt-28">
+          <div class="text-left">
             <div class="flex">
-              <img alt="MoBot Logo" class="h-20 md:h-36 w-auto mr-6" src="/robot.svg" />
-              <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                <span class="block xl:inline">The only Discord bot</span>
-                {{ ' ' }}
-                <span class="block text-indigo-600 xl:inline">you need</span>
+              <img alt="MoBot Logo" class="h-24 md:h-36 w-auto mr-6" src="/robot.svg" />
+              <h1 class="text-4xl font-bold tracking-tight text-mobot-dark-gray-300 sm:text-5xl md:text-6xl">
+                <span class="inline">The only Discord bot</span>
+                <p>
+                  <span class="text-mobot-purple-400 inline">you need</span>
+                </p>
               </h1>
             </div>
-            <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Imagine if you could have one Discord bot to manage role reactions, pronoun picking, custom commands, streaming alerts, and more! All at a fraction of the cost compared to the competition.</p>
-            <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">MoBot is coming soon...</p>
+            <p class="mt-3 text-base text-mobot-dark-gray-200 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Imagine if you could have one Discord bot to manage role reactions, pronoun picking, custom commands, streaming alerts, and more! All at a fraction of the cost compared to the competition.</p>
+            <p class="mt-3 text-base text-mobot-dark-gray-200 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">MoBot is coming soon...</p>
             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div class="rounded-md shadow">
-                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Add Bot</a>
+                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-mobot-purple-300 px-8 py-3 text-base font-medium text-white hover:bg-mobot-purple-400 md:py-4 md:px-10 md:text-lg">Add Bot</a>
               </div>
               <div class="mt-3 sm:mt-0 sm:ml-3">
-                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg">Learn More</a>
+                <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-mobot-light-purple-100 px-8 py-3 text-base font-medium text-mobot-light-purple-400 hover:bg-mobot-light-purple-200 md:py-4 md:px-10 md:text-lg">Learn More</a>
               </div>
             </div>
           </div>
         </main>
       </div>
-    </div>
-    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-      <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full" src="https://safety.discord.com/hc/article_attachments/360059655071/platforms.png" alt="Bot example image" />
     </div>
   </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,30 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mobot-blue-100' : '#C2FFFF',
+        'mobot-blue-200' : '#8DFDFF',
+        'mobot-blue-300' : '#60FFFF',
+        'mobot-blue-400' : '#33FCFC',
+        'mobot-light-purple-100' : '#FBFBFF',
+        'mobot-light-purple-200' : '#DCDAF9',
+        'mobot-light-purple-300' : '#B6B2EB',
+        'mobot-light-purple-400' : '#8D87D3',
+        'mobot-purple-100' : '#FCF9FF',
+        'mobot-purple-200' : '#E5D1F9',
+        'mobot-purple-300' : '#C8A6EB',
+        'mobot-purple-400' : '#A679D3',
+        'mobot-gray-100' : '#F2F2F2',
+        'mobot-gray-200' : '#C3C3C3',
+        'mobot-gray-300' : '#989898',
+        'mobot-gray-400' : '#696868',
+        'mobot-dark-gray-100' : '#999898',
+        'mobot-dark-gray-200' : '#666666',
+        'mobot-dark-gray-300' : '#2D2D2D',
+        'mobot-dark-gray-400' : '#030202'
+      },
+    },
   },
   plugins: [],
 }
